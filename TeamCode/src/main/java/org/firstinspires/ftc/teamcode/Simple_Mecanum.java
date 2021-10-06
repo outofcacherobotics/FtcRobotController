9e34bb9042b0f471e94201b740d8d42fb22c5ef6
lukerhoads <pumpkineater661@gmail.com>
2021-10-06 15:54:52 -0500

    SimpleMecanum rewrite to use Controls

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Simple_Mecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Simple_Mecanum.java
@@ -6,50 +6,36 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp
 public class Simple_Mecanum extends LinearOpMode {
-
-    private DcMotor frontLeft;
-    private DcMotor backLeft;
-    private DcMotor frontRight;
-    private DcMotor backRight;
+    private DcMotor left_front_motor, right_front_motor, left_back_motor, right_back_motor;
+    private Controls controls;
 
     @Override
     public void runOpMode() {
-        float x;
-        float y;
-        float clockwise;
-        double fl;
-        double fr;
-        double bl;
-        double br;
+        left_front_motor = hardwareMap.get(DcMotor.class, "frontLeft");
+        right_front_motor = hardwareMap.get(DcMotor.class, "frontRight");
+        left_back_motor = hardwareMap.get(DcMotor.class, "backLeft");
+        right_back_motor = hardwareMap.get(DcMotor.class, "backRight");
+        right_front_motor.setDirection(DcMotor.Direction.REVERSE);
+        right_back_motor.setDirection(DcMotor.Direction.REVERSE);
 
-        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
-        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
-        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
-        backRight = hardwareMap.get(DcMotor.class, "backRight");
-        frontRight.setDirection(DcMotor.Direction.REVERSE);
-        backRight.setDirection(DcMotor.Direction.REVERSE);
+        controls = new Controls(
+            gamepad1, 
+            left_front_motor,
+            right_front_motor,
+            left_back_motor,
+            right_back_motor,
+        );
 
         waitForStart();
         if (opModeIsActive()) {
-            frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-            backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-            frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-            backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            controls.setPowerBehavior();
             while (opModeIsActive()) {
-                x = gamepad1.left_stick_x;
-                y = gamepad1.left_stick_y;
-
-                clockwise = gamepad1.right_stick_x;
-
-                fl = y - clockwise - x;
-                fr = y + clockwise - x;
-                bl = y - clockwise + x;
-                br = y + clockwise + x;
+                controls.update()
                 
-                frontLeft.setPower(fl);
-                frontRight.setPower(fr);
-                backLeft.setPower(bl);
-                backRight.setPower(br);
+                left_front_motor.setPower(controls.getLeftFrontPower());
+                right_front_motor.setPower(controls.getRightFrontPower());
+                left_back_motor.setPower(controls.getLeftBackPower());
+                right_back_motor.setPower(controls.getRightBackPower());
 
                 telemetry.update();
             }
