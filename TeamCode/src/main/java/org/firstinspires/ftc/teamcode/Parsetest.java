eebb74c659c5b0f00c55036842c7c6763dc549ac
gsrgithub1 <92129505+gsrgithub1@users.noreply.github.com>
2021-11-01 23:31:58 -0500

    JSON robot status

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Parsetest.java
@@ -0,0 +1,28 @@
+package main.java.org.firstinspires.ftc.teamcode;
+
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import static org.junit.Assert.*;
+import org.json.*;
+
+Class main{
+    public static void main(String[] args){
+        boolean robotRunning = Status.getRobotStatus(running);
+            if (robotRunning) {
+                String statusJsonString = '{
+                    "running": true
+                  }';
+            } else {
+                String statusJsonString = '{
+                    "running": false
+                  }';
+            }
+    }
+    /* creates boolean robotRunning from getRobotStatus, robotRunning being true creates "running: true" JSON string
+       otherwise "running: false" string is created
+       likely not going to work first attempts, fix during meet if screwed up
+       */
+}
